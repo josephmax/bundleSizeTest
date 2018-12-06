c15f6b891a1799380e28b5e147616382f21f9f54
Joseph Cheung <josephz2314@hotmail.com>
2018-12-06 22:30:28 +0800

    react imported 116k bundle.js 4k main.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function APP () {
+    return <div>hello ant</div>
+}
+
+ReactDOM.render(
+    <APP />,
+    document.getElementById('app')
+)
